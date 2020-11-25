757b99d0b3f04ff9ab2b7815fe14dbf2e3011954
jorgecristina <jorgelemos.cristina@gmail.com>
2020-11-25 04:00:14 +0000

    New Pass

--- LoginVerification/src/main/java/com/jcristin/loginverification/LoginVerification.java
+++ LoginVerification/src/main/java/com/jcristin/loginverification/LoginVerification.java
@@ -2,6 +2,6 @@ package com.jcristin.loginverification;
 
 public class LoginVerification {
     public boolean verifyEmailAndPassword(String email, String password) {
-        return email.equals("test@test.com") && password.equals("1234");
+        return email.equals("test@test.com") && password.equals("12345678");
     }
 }
