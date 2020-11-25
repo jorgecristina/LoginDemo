0bf7e5ed1766eb86a89f3939088746a8914653b8
jorgecristina <jorgelemos.cristina@gmail.com>
2020-11-25 03:11:45 +0000

    Change method name 2

--- LoginVerification/src/main/java/com/jcristin/loginverification/LoginVerification.java
+++ LoginVerification/src/main/java/com/jcristin/loginverification/LoginVerification.java
@@ -1,7 +1,7 @@
 package com.jcristin.loginverification;
 
 public class LoginVerification {
-    public boolean verify(String email, String password) {
+    public boolean verifyEmailAndPassword(String email, String password) {
         return email.equals("test@test.com") && password.equals("1234");
     }
 }
--- app/src/test/java/com/jcristin/myapplication/ExampleUnitTest.java
+++ app/src/test/java/com/jcristin/myapplication/ExampleUnitTest.java
@@ -18,17 +18,17 @@ public class ExampleUnitTest {
 
     @Test
     public void wrongPassword() {
-        assertFalse(lv.verify("test@test.com", "blabla"));
+        assertFalse(lv.verifyEmailAndPassword("test@test.com", "blabla"));
     }
 
     @Test
     public void wrongEmail() {
-        assertFalse(lv.verify("test@test.xxx", "1234"));
+        assertFalse(lv.verifyEmailAndPassword("test@test.xxx", "1234"));
     }
 
 
     @Test
     public void allOK() {
-        assertTrue(lv.verify("test@test.com", "1234"));
+        assertTrue(lv.verifyEmailAndPassword("test@test.com", "1234"));
     }
 }
