994b3d96bfccc6d4d81d81b920091a44a60b6fe0
jorgecristina <jorgelemos.cristina@gmail.com>
2020-11-25 03:05:13 +0000

    Password change 2

--- LoginVerification/src/main/java/com/jcristin/loginverification/LoginVerification.java
+++ LoginVerification/src/main/java/com/jcristin/loginverification/LoginVerification.java
@@ -2,6 +2,7 @@ package com.jcristin.loginverification;
 
 public class LoginVerification {
     public boolean verify(String email, String password) {
-        return email.equals("test@test.com") && password.equals("123456");
+        return email.equals("test@test.com") && password.equals("123456")
+                || (email.equals("admin@test.com") && password.equals("123456"));
     }
 }
